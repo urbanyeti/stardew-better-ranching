d9664122259f3abbab0d87d2d8437b67ef149f80
urbanyeti <urbanski@gmail.com>
2017-12-16 17:42:37 -0800

    Add config file, fix bugs

---
+++ BetterRanching/Config.cs
@@ -0,0 +1,10 @@
+﻿namespace BetterRanching
+{
+	internal class Config
+	{
+		public bool PreventFailedHarvesting { get; internal set; } = true;
+		public bool PreventHarvestRepeating { get; internal set; } = true;
+		public bool DisplayHearts { get; internal set; } = true;
+		public bool DisplayProduce { get; internal set; } = true;
+	}
+}
