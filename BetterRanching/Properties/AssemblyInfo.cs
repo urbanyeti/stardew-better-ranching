2e287735086b0646e2c9ae758d0d08270a18d344
urbanyeti <urbanski@gmail.com>
2020-02-10 17:48:03 -0800

    Updated version

--- BetterRanching/Properties/AssemblyInfo.cs
+++ BetterRanching/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@
 [assembly: AssemblyTitle("BetterRanching")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("Urbanyeti")]
-[assembly: AssemblyVersion("1.7.5")]
-[assembly: AssemblyFileVersion("1.7.5")]
+[assembly: AssemblyVersion("1.7.6")]
+[assembly: AssemblyFileVersion("1.7.6")]
