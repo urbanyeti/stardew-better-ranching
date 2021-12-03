9e0f4675b7c795dcf6518e7d0c2ffaffde8cf47a
Daniel Urbanski <urbanski@gmail.com>
2021-12-02 17:48:55 -0800

    - Update to .Net5 - Add support GenericModConfigMenu

--- BetterRanching/BetterRanching.cs
+++ BetterRanching/BetterRanching.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using GenericModConfigMenu;
 using Microsoft.Xna.Framework;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
@@ -9,12 +10,12 @@ using StardewValley.Characters;
 
 namespace BetterRanching
 {
-    /// <summary>The mod entry class loaded by SMAPI.</summary>
-    public class BetterRanching : Mod
+	/// <summary>The mod entry class loaded by SMAPI.</summary>
+	public class BetterRanching : Mod
 	{
 		private FarmAnimal AnimalBeingRanched { get; set; }
 		private ModConfig Config { get; set; }
-		private BetterRanchingApi Api {get; set; }
+		private BetterRanchingApi Api { get; set; }
 
 		/// <summary>The mod entry point, called after the mod is first loaded.</summary>
 		/// <param name="helper">Provides simplified APIs for writing mods.</param>
@@ -25,11 +26,55 @@ namespace BetterRanching
 			helper.Events.GameLoop.UpdateTicked += OnUpdateTicked;
 			helper.Events.Display.RenderedWorld += OnRenderedWorld;
 			helper.Events.Input.ButtonPressed += OnButtonPressed;
+			helper.Events.GameLoop.GameLaunched += OnGameLaunched;
 		}
 
 		public override object GetApi()
 		{
 			return new BetterRanchingApi(Config);
+		}
+
+		/// <summary>
+		/// Raised after the game is launched, right before the first update tick. This happens once per game session (unrelated to loading saves). All mods are loaded and initialised at this point, so this is a good time to set up mod integrations. 
+		/// </summary>
+		private void OnGameLaunched(object sender, GameLaunchedEventArgs e)
+		{
+			// get Generic Mod Config Menu's API (if it's installed)
+			var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+			if (configMenu is null)
+			{
+				return;
+			}
+
+			// register mod
+			configMenu.Register(
+				mod: ModManifest,
+				reset: () => Config = new ModConfig(),
+				save: () => Helper.WriteConfig(Config)
+			);
+
+			// add some config options
+			configMenu.AddBoolOption(
+				mod: ModManifest,
+				name: () => "Prevent Failed Harvesting",
+				tooltip: () => "Prevents the failed milking/shearing animation and sound effect if no valid animal is selected. Note: Disable this if using the 'Tap-to-move & Auto-Attack' control scheme on Android.",
+				getValue: () => Config.PreventFailedHarvesting,
+				setValue: value => Config.PreventFailedHarvesting = value
+			);
+			configMenu.AddBoolOption(
+				mod: ModManifest,
+				name: () => "Display Hearts",
+				tooltip: () => "Display hearts above animals that have not yet been petted.",
+				getValue: () => Config.DisplayHearts,
+				setValue: value => Config.DisplayHearts = value
+			);
+			configMenu.AddBoolOption(
+				mod: ModManifest,
+				name: () => "Display Produce",
+				tooltip: () => "Displays produce above animal if it is ready to be harvested.",
+				getValue: () => Config.DisplayProduce,
+				setValue: value => Config.DisplayProduce = value
+			);
 		}
 
 		/// <summary>Raised after the game state is updated (≈60 times per second).</summary>
--- BetterRanching/ModConfig.cs
+++ BetterRanching/ModConfig.cs
@@ -3,7 +3,6 @@
 	internal class ModConfig
 	{
 		public bool PreventFailedHarvesting { get; set; } = true;
-		public bool PreventHarvestRepeating { get; set; } = true;
 		public bool DisplayHearts { get; set; } = true;
 		public bool DisplayProduce { get; set; } = true;
 	}
